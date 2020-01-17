fcd4a087f2466f90e345d049dc54b6230d2319a2
unknown <holmanryan4@gmail.com>
2020-01-17 13:26:40 -0600

    working on the store class trying to finish the list of items the store will sell

--- Lemonade Stand Game/Store.cs
+++ Lemonade Stand Game/Store.cs
@@ -10,10 +10,11 @@ namespace Lemonade_Stand_Game
     {
 
         //Member Variables (HAS A)
-        private double pricePerLemon;
-        private double pricePerSugarCube;
-        private double pricePerIceCube;
-        private double pricePerCup;
+       public double pricePerLemon;
+        public double pricePerSugarCube;
+       public double pricePerIceCube;
+       public double pricePerCup;
+        public string stockUpOption;
         //Constructor
         public Store()
         {
@@ -21,12 +22,24 @@ namespace Lemonade_Stand_Game
             pricePerSugarCube = 0.06;
             pricePerIceCube = 0.04;
             pricePerCup = 0.03;
-
+            
 
         }
 
         //Member Methods (CAN DO)
+        public void VisitStore(Player player)
+        {
+            Console.WriteLine(player.name + "would you like to stock up on supplies? type yes or no to continue.");
+            string stockUpOption = Console.ReadLine();
+            if (stockUpOption == "yes")
+            {
+                Console.WriteLine("Great! What would you like to buy? 1. cups, 2. lemons, 3.sugar cubes, 4. ice cubes" );
 
+            }
+            
+                
+            
+        }
 
     }
 }
